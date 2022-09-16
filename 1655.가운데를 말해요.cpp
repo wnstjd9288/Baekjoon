44caa973d3be23eafaf4da2646ae36e4d9fd774d
junmok <42504239+wnstjd9288@users.noreply.github.com>
2022-09-17 08:03:42 +0900

    Create 1655.가운데를 말해요.cpp

---
+++ 1655.가운데를 말해요.cpp
@@ -0,0 +1,101 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int min_heap[100001] = { 0, };
+int max_heap[100001] = { 0, };
+int min_cnt = 0;
+int max_cnt = 0;
+int heap[101];
+void min_push(int x) {
+    min_heap[++min_cnt] = x;
+    int child = min_cnt;
+    int parent = child / 2;
+
+    while (child > 1 && min_heap[child] < min_heap[parent]) {
+        int temp = min_heap[child];
+        min_heap[child] = min_heap[parent];
+        min_heap[parent] = temp;
+
+        child = parent;
+        parent = child / 2;
+    }
+}
+int min_pop() {
+    if (min_cnt == 0) return 0;
+    int r = min_heap[1];
+    min_heap[1] = min_heap[min_cnt];
+    min_cnt--;
+    int parent = 1;
+    int child = parent * 2;
+
+    if (child + 1 <= min_cnt) {
+        child = min_heap[child] < min_heap[child + 1] ? child : child + 1;
+    }
+    while (child <= min_cnt && min_heap[child] < min_heap[parent]) {
+        int temp = min_heap[child];
+        min_heap[child] = min_heap[parent];
+        min_heap[parent] = temp;
+
+        parent = child;
+        child = parent * 2;
+
+        if (child + 1 <= min_cnt) {
+            child = min_heap[child] < min_heap[child + 1] ? child : child + 1;
+        }
+    }
+    return r;
+}
+void max_push(int x) {
+    max_heap[++max_cnt] = x;
+    int child = max_cnt;
+    int parent = child / 2;
+
+    while (child > 1 && max_heap[child] > max_heap[parent]) {
+        int temp = max_heap[child];
+        max_heap[child] = max_heap[parent];
+        max_heap[parent] = temp;
+
+        child = parent;
+        parent = child / 2;
+    }
+}
+int max_pop() {
+    if (max_cnt == 0) return 0;
+    int r = max_heap[1];
+    max_heap[1] = max_heap[max_cnt];
+    max_cnt--;
+    int parent = 1;
+    int child = parent * 2;
+
+    if (child + 1 <= max_cnt) {
+        child = max_heap[child] > max_heap[child + 1] ? child : child + 1;
+    }
+    while (child <= max_cnt && max_heap[child] > max_heap[parent]) {
+        int temp = max_heap[child];
+        max_heap[child] = max_heap[parent];
+        max_heap[parent] = temp;
+
+        parent = child;
+        child = parent * 2;
+
+        if (child + 1 <= max_cnt) {
+            child = max_heap[child] > max_heap[child + 1] ? child : child + 1;
+        }
+    }
+    return r;
+}
+int main() {
+
+    int n;
+    scanf("%d", &n);
+    printf("%d", min_cnt);
+    for (int i = 0; i < n; i++) {
+        int x;
+        scanf("%d", &x);
+        min_push(x);
+    }
+    for (int i = 0; i < min_cnt; i++) {
+        printf("%d ", min_heap[i]);
+    }
+    return 0;
+}
